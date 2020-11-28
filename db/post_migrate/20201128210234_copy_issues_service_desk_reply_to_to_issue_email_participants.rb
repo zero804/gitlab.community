ac0adaf0d67a2805966f4143961b49bc1a27261d
Lee Tickett <lee@tickett.net>
2020-11-28 21:27:17 +0000

    Added migration to issue_email_participants

---
+++ db/post_migrate/20201128210234_copy_issues_service_desk_reply_to_to_issue_email_participants.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class CopyIssuesServiceDeskReplyToToIssueEmailParticipants < ActiveRecord::Migration[6.0]
+  DOWNTIME = false
+
+  def up
+    gitlab_support_bot_id = execute("SELECT id FROM users WHERE username = 'support-bot'").first["id"]
+
+    execute <<~SQL.squish
+      INSERT INTO issue_email_participants (issue_id,
+        created_at,
+        updated_at,
+        email)
+      SELECT id,
+        created_at,
+        updated_at,
+        service_desk_reply_to
+      FROM issues
+      WHERE author_id = #{gitlab_support_bot_id}
+      ON CONFLICT (issue_id, email) DO NOTHING
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      DELETE FROM issue_email_participants
+    SQL
+  end
+end
