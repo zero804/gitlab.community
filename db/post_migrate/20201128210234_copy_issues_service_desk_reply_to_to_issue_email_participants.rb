4bb3e825b485183de37bb99c6dcf055463c5c4fb
Lee Tickett <lee@tickett.net>
2020-12-02 08:37:42 +0000

    Keying on `service_desk_reply_to` as more efficient and removed `down` migration

--- db/post_migrate/20201128210234_copy_issues_service_desk_reply_to_to_issue_email_participants.rb
+++ db/post_migrate/20201128210234_copy_issues_service_desk_reply_to_to_issue_email_participants.rb
@@ -4,8 +4,6 @@ class CopyIssuesServiceDeskReplyToToIssueEmailParticipants < ActiveRecord::Migra
   DOWNTIME = false
 
   def up
-    gitlab_support_bot_id = execute("SELECT COALESCE((SELECT id FROM users WHERE username = 'support-bot'), -1) id").first["id"]
-
     execute <<~SQL.squish
       INSERT INTO issue_email_participants (issue_id,
         created_at,
@@ -16,14 +14,12 @@ class CopyIssuesServiceDeskReplyToToIssueEmailParticipants < ActiveRecord::Migra
         updated_at,
         service_desk_reply_to
       FROM issues
-      WHERE author_id = #{gitlab_support_bot_id}
+      WHERE service_desk_reply_to IS NOT NULL
       ON CONFLICT (issue_id, email) DO NOTHING
     SQL
   end
 
   def down
-    execute <<~SQL
-      DELETE FROM issue_email_participants
-    SQL
+     # no-op
   end
 end
