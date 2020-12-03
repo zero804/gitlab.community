7e205184979f39f14c02a3adef24f640264f4b6a
mfluharty <mfluharty@gitlab.com>
2020-12-02 18:09:41 -0700

    Remove testing from ineligible_total_categories

--- spec/lib/gitlab/usage_data_spec.rb
+++ spec/lib/gitlab/usage_data_spec.rb
@@ -1243,7 +1243,7 @@ RSpec.describe Gitlab::UsageData, :aggregate_failures do
     subject { described_class.redis_hll_counters }
 
     let(:categories) { ::Gitlab::UsageDataCounters::HLLRedisCounter.categories }
-    let(:ineligible_total_categories) { %w[source_code testing ci_secrets_management incident_management_alerts snippets] }
+    let(:ineligible_total_categories) { %w[source_code ci_secrets_management incident_management_alerts snippets] }
 
     it 'has all known_events' do
       expect(subject).to have_key(:redis_hll_counters)
