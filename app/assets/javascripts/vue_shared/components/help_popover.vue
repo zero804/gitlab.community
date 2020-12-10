4a9190d1c64d1211833e0e3b2e6879feaa2f1cc3
Veethika <veethika.m@gmail.com>
2020-12-10 18:56:29 +0530

    Replaced bootstrap popover with GlPopover

--- app/assets/javascripts/vue_shared/components/help_popover.vue
+++ app/assets/javascripts/vue_shared/components/help_popover.vue
@@ -1,8 +1,5 @@
 <script>
-import $ from 'jquery';
-import { GlIcon } from '@gitlab/ui';
-import { inserted } from '~/feature_highlight/feature_highlight_helper';
-import { mouseenter, debouncedMouseleave, togglePopover } from '~/shared/popover';
+import { GlIcon, GlPopover } from '@gitlab/ui';
 
 /**
  * Render a button with a question mark icon
@@ -11,7 +8,7 @@ import { mouseenter, debouncedMouseleave, togglePopover } from '~/shared/popover
 export default {
   name: 'HelpPopover',
   components: {
-    GlIcon,
+    GlIcon, GlPopover,
   },
   props: {
     options: {
@@ -20,30 +17,18 @@ export default {
       default: () => ({}),
     },
   },
-  mounted() {
-    const $el = $(this.$el);
-
-    $el
-      .popover({
-        html: true,
-        trigger: 'focus',
-        container: 'body',
-        placement: 'top',
-        template:
-          '<div class="popover" role="tooltip"><div class="arrow"></div><p class="popover-header"></p><div class="popover-body"></div></div>',
-        ...this.options,
-      })
-      .on('mouseenter', mouseenter)
-      .on('mouseleave', debouncedMouseleave(300))
-      .on('inserted.bs.popover', inserted)
-      .on('show.bs.popover', () => {
-        window.addEventListener('scroll', togglePopover.bind($el, false), { once: true });
-      });
-  },
+  
 };
 </script>
 <template>
-  <button type="button" class="btn btn-blank btn-transparent btn-help" tabindex="0">
-    <gl-icon name="question" />
-  </button>
+  <span>
+    <button ref="popoverTrigger" type="button" class="btn btn-blank btn-transparent btn-help" tabindex="0">
+      <gl-icon name="question" />
+    </button>
+    <gl-popover triggers="hover focus" :target="() => $refs.popoverTrigger">
+      <template #title>{{options.title}}</template>
+      {{options.content}}
+    </gl-popover>
+  </span>
+
 </template>
