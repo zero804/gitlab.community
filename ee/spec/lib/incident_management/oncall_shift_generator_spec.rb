332305f33b1b57459ff65697a20a9ecc6eaedf4d
syasonik <syasonik@gitlab.com>
2020-12-15 15:47:49 -0500

    Swap to multiple shift generation methods

--- ee/spec/lib/incident_management/oncall_shift_generator_spec.rb
+++ ee/spec/lib/incident_management/oncall_shift_generator_spec.rb
@@ -7,20 +7,27 @@ RSpec.describe IncidentManagement::OncallShiftGenerator do
   let_it_be(:rotation) { create(:incident_management_oncall_rotation, starts_at: rotation_start_time) }
 
   let(:current_time) { Time.parse('2020-12-08 15:00:00').utc }
-  let(:starts_at) { Time.parse('2020-12-08 02:00:00').utc }
-  let(:ends_at) { starts_at + (shift_length * 2) }
   let(:shift_length) { rotation.shift_duration }
-
-  subject(:shifts) { described_class.new(rotation, starts_at: starts_at, ends_at: ends_at).execute }
 
   around do |example|
     travel_to(current_time) { example.run }
+  end
+
+  shared_context 'with three participants' do
+    let_it_be(:participants) do
+      {
+        blue200: create(:incident_management_oncall_participant, :with_access, rotation: rotation, color_palette: :blue, color_weight: '200'),
+        blue50: create(:incident_management_oncall_participant, :with_access, rotation: rotation, color_palette: :blue, color_weight: '50'),
+        magenta: create(:incident_management_oncall_participant, :with_access, rotation: rotation, color_palette: :magenta)
+      }
+    end
   end
 
   # shift_params takes format [[participant identifier, start_time, end_time]]
   shared_examples 'unsaved rotations' do |shift_params|
     it 'has the expected attributes' do
       expect(shifts).to all( be_a(IncidentManagement::OncallShift) )
+      expect(shifts.length).to eq(shift_params.length)
 
       shifts.each_with_index do |shift, idx|
         expect(shift).to have_attributes(
@@ -31,110 +38,154 @@ RSpec.describe IncidentManagement::OncallShiftGenerator do
           ends_at: Time.parse(shift_params[idx][2]).utc
         )
       end
-      expect(shifts.length).to eq(shift_params.length)
     end
   end
 
-  context 'with no participants' do
-    it { is_expected.to be_empty }
+  shared_examples 'unsaved rotation' do |shift_params|
+    let(:shifts) { [shift] }
+
+    include_examples 'unsaved rotations', [shift_params]
   end
 
-  context 'with one participant' do
-    let_it_be(:participants) { { user: create(:incident_management_oncall_participant, :with_access, rotation: rotation) } }
+  describe '#for_timeframe' do
+    let(:starts_at) { Time.parse('2020-12-08 02:00:00').utc }
+    let(:ends_at) { starts_at + (shift_length * 2) }
 
-    # Expect 3 shifts of 5 days starting, both for the same user
-    it_behaves_like 'unsaved rotations', [
-      [:user, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
-      [:user, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
-      [:user, '2020-12-18 00:00:00', '2020-12-23 00:00:00']
-    ]
-  end
+    subject(:shifts) { described_class.new(rotation).for_timeframe(starts_at: starts_at, ends_at: ends_at) }
 
-  context 'with many participants' do
-    let_it_be(:participants) do
-      {
-        blue200: create(:incident_management_oncall_participant, :with_access, rotation: rotation, color_palette: :blue, color_weight: '200'),
-        blue50: create(:incident_management_oncall_participant, :with_access, rotation: rotation, color_palette: :blue, color_weight: '50'),
-        magenta: create(:incident_management_oncall_participant, :with_access, rotation: rotation, color_palette: :magenta)
-      }
-    end
-
-    context 'when end time is earlier than start time' do
-      let(:ends_at) { starts_at - 1.hour }
-
+    context 'with no participants' do
       it { is_expected.to be_empty }
     end
 
-    context 'when start time is the same time as the rotation start time' do
-      let(:starts_at) { rotation_start_time }
+    context 'with one participant' do
+      let_it_be(:participants) { { user: create(:incident_management_oncall_participant, :with_access, rotation: rotation) } }
 
-      # Expect 2 shifts of 5 days starting with first user
-      # at rotation start time
+      # Expect 3 shifts of 5 days starting, both for the same user
       it_behaves_like 'unsaved rotations', [
-        [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
-        [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00']
+        [:user, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
+        [:user, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
+        [:user, '2020-12-18 00:00:00', '2020-12-23 00:00:00']
       ]
     end
 
-    context 'when start time is earlier than the rotation start time' do
-      let(:starts_at) { 1.day.before(rotation_start_time) }
+    context 'with many participants' do
+      include_context 'with three participants'
 
-      # Expect 2 shifts of 5 days starting with first user
-      # at rotation start time
-      it_behaves_like 'unsaved rotations', [
-        [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
-        [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00']
-      ]
+      context 'when end time is earlier than start time' do
+        let(:ends_at) { starts_at - 1.hour }
+
+        it { is_expected.to be_empty }
+      end
+
+      context 'when start time is the same time as the rotation start time' do
+        let(:starts_at) { rotation_start_time }
+
+        # Expect 2 shifts of 5 days starting with first user
+        # at rotation start time
+        it_behaves_like 'unsaved rotations', [
+          [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
+          [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00']
+        ]
+      end
+
+      context 'when start time is earlier than the rotation start time' do
+        let(:starts_at) { 1.day.before(rotation_start_time) }
+
+        # Expect 2 shifts of 5 days starting with first user
+        # at rotation start time
+        it_behaves_like 'unsaved rotations', [
+          [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
+          [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00']
+        ]
+      end
+
+      context 'when start time coincides with a shift change' do
+        let(:starts_at) { rotation_start_time + shift_length }
+
+        # Expect 2 shift of 5 days, starting with second user
+        # starting at 2nd shift
+        it_behaves_like 'unsaved rotations', [
+          [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
+          [:magenta, '2020-12-18 00:00:00', '2020-12-23 00:00:00']
+        ]
+      end
+
+      context 'when start time is partway through a shift' do
+        let(:starts_at) { rotation_start_time + (0.6 * shift_length) }
+
+        # Expect 3 shifts of 5 days starting with first user
+        # which includes partial shift
+        it_behaves_like 'unsaved rotations', [
+          [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
+          [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
+          [:magenta, '2020-12-18 00:00:00', '2020-12-23 00:00:00']
+        ]
+      end
+
+      context 'when the rotation has been completed many times over' do
+        let(:starts_at) { rotation_start_time + 7.weeks }
+
+        # Expect 3 shifts of 5 days starting with first user,
+        # starting 7 weeks out
+        it_behaves_like 'unsaved rotations', [
+          [:blue50, '2021-01-22 00:00:00', '2021-01-27 00:00:00'],
+          [:blue200, '2021-01-27 00:00:00', '2021-02-01 00:00:00'],
+          [:magenta, '2021-02-01 00:00:00', '2021-02-06 00:00:00']
+        ]
+      end
+
+      context 'when timeframe covers the rotation many times over' do
+        let(:ends_at) { starts_at + (shift_length * 6.8) }
+
+        # Expect 7 shifts of 5 days starting with first user
+        it_behaves_like 'unsaved rotations', [
+          [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
+          [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
+          [:magenta, '2020-12-18 00:00:00', '2020-12-23 00:00:00'],
+          [:blue50, '2020-12-23 00:00:00', '2020-12-28 00:00:00'],
+          [:blue200, '2020-12-28 00:00:00', '2021-01-02 00:00:00'],
+          [:magenta, '2021-01-02 00:00:00', '2021-01-07 00:00:00'],
+          [:blue50, '2021-01-07 00:00:00', '2021-01-12 00:00:00']
+        ]
+      end
+    end
+  end
+
+  describe '#for_timestamp' do
+    let(:timestamp) { Time.current }
+
+    subject(:shift) { described_class.new(rotation).for_timestamp(timestamp) }
+
+    context 'with no participants' do
+      it { is_expected.to be_nil }
     end
 
-    context 'when start time coincides with a shift change' do
-      let(:starts_at) { rotation_start_time + shift_length }
+    context 'with participants' do
+      include_context 'with three participants'
 
-      # Expect 2 shift of 5 days, starting with second user
-      # starting at 2nd shift
-      it_behaves_like 'unsaved rotations', [
-        [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
-        [:magenta, '2020-12-18 00:00:00', '2020-12-23 00:00:00']
-      ]
-    end
+      context 'when timestamp is before the rotation start time' do
+        let(:timestamp) { rotation_start_time - 10.minutes }
 
-    context 'when start time is partway through a shift' do
-      let(:starts_at) { rotation_start_time + (0.6 * shift_length) }
+        it { is_expected.to be_nil }
+      end
 
-      # Expect 3 shifts of 5 days starting with first user
-      # which includes partial shift
-      it_behaves_like 'unsaved rotations', [
-        [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
-        [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
-        [:magenta, '2020-12-18 00:00:00', '2020-12-23 00:00:00']
-      ]
-    end
+      context 'when timestamp matches the rotation start time' do
+        let(:timestamp) { rotation_start_time }
 
-    context 'when the rotation has been completed many times over' do
-      let(:starts_at) { rotation_start_time + 7.weeks }
+        it_behaves_like 'unsaved rotation', [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00']
+      end
 
-      # Expect 3 shifts of 5 days starting with first user,
-      # starting 7 weeks out
-      it_behaves_like 'unsaved rotations', [
-        [:blue50, '2021-01-22 00:00:00', '2021-01-27 00:00:00'],
-        [:blue200, '2021-01-27 00:00:00', '2021-02-01 00:00:00'],
-        [:magenta, '2021-02-01 00:00:00', '2021-02-06 00:00:00']
-      ]
-    end
+      context 'when timestamp matches a shift start/end time' do
+        let(:timestamp) { rotation_start_time + shift_length }
 
-    context 'when timeframe covers the rotation many times over' do
-      let(:ends_at) { starts_at + (shift_length * 6.8) }
+        it_behaves_like 'unsaved rotation', [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00']
+      end
 
-      # Expect 7 shifts of 5 days starting with first user
-      it_behaves_like 'unsaved rotations', [
-        [:blue50, '2020-12-08 00:00:00', '2020-12-13 00:00:00'],
-        [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00'],
-        [:magenta, '2020-12-18 00:00:00', '2020-12-23 00:00:00'],
-        [:blue50, '2020-12-23 00:00:00', '2020-12-28 00:00:00'],
-        [:blue200, '2020-12-28 00:00:00', '2021-01-02 00:00:00'],
-        [:magenta, '2021-01-02 00:00:00', '2021-01-07 00:00:00'],
-        [:blue50, '2021-01-07 00:00:00', '2021-01-12 00:00:00']
-      ]
+      context 'when timestamp is in the middle of a shift' do
+        let(:timestamp) { rotation_start_time + (1.6 * shift_length) }
+
+        it_behaves_like 'unsaved rotation', [:blue200, '2020-12-13 00:00:00', '2020-12-18 00:00:00']
+      end
     end
   end
 end
