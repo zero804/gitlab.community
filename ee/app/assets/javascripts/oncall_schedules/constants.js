621e415b849e7b3540720877a3797e8582ed08cb
David O'Regan <doregan@gitlab.com>
2020-12-17 14:48:04 +0000

    Enable Rotation creation
    Enable rotation creation in
    on call schedules via
    GraphQL with supporting specs

--- ee/app/assets/javascripts/oncall_schedules/constants.js
+++ ee/app/assets/javascripts/oncall_schedules/constants.js
@@ -1,9 +1,11 @@
 export const LENGTH_ENUM = {
-  hours: 'hours',
-  days: 'days',
-  weeks: 'weeks',
+  hours: 'HOURS',
+  days: 'DAYS',
+  weeks: 'WEEKS',
 };
 
 export const CHEVRON_SKIPPING_SHADE_ENUM = ['500', '600', '700', '800', '900', '950'];
 
 export const CHEVRON_SKIPPING_PALETTE_ENUM = ['blue', 'orange', 'aqua', 'green', 'magenta'];
+
+export const HOURS_IN_DAY = 24;
--- ee/spec/frontend/oncall_schedule/rotations/add_rotation_modal_spec.js
+++ ee/spec/frontend/oncall_schedule/rotations/add_rotation_modal_spec.js
@@ -3,16 +3,17 @@ import createMockApollo from 'jest/helpers/mock_apollo_helper';
 import VueApollo from 'vue-apollo';
 import waitForPromises from 'helpers/wait_for_promises';
 import { GlDropdownItem, GlModal, GlAlert, GlTokenSelector } from '@gitlab/ui';
-import { addRotationModalId } from 'ee/oncall_schedules/components/oncall_schedule';
+import { addRotationModalId } from 'ee/oncall_schedules/components/oncall_schedule.vue';
 import AddRotationModal from 'ee/oncall_schedules/components/rotations/add_rotation_modal.vue';
-// import createOncallScheduleRotationMutation from 'ee/oncall_schedules/graphql/create_oncall_schedule_rotation.mutation.graphql';
 import usersSearchQuery from '~/graphql_shared/queries/users_search.query.graphql';
-import { participants } from '../mocks/apollo_mock';
+import { participants, getOncallSchedulesQueryResponse } from '../mocks/apollo_mock';
 
 const localVue = createLocalVue();
 const projectPath = 'group/project';
 const mutate = jest.fn();
 const mockHideModal = jest.fn();
+const schedule =
+  getOncallSchedulesQueryResponse.data.project.incidentManagementOncallSchedules.nodes[0];
 
 localVue.use(VueApollo);
 
@@ -37,6 +38,7 @@ describe('AddRotationModal', () => {
       propsData: {
         modalId: addRotationModalId,
         ...props,
+        schedule,
       },
       provide: {
         projectPath,
@@ -62,6 +64,7 @@ describe('AddRotationModal', () => {
       localVue,
       propsData: {
         modalId: addRotationModalId,
+        schedule,
       },
       apolloProvider: fakeApollo,
       data() {
@@ -162,18 +165,18 @@ describe('AddRotationModal', () => {
   });
 
   describe('Rotation create', () => {
-    it('makes a request with `oncallScheduleRotationCreate` to create a schedule rotation', () => {
+    it('makes a request with `oncallRotationCreate` to create a schedule rotation', () => {
       mutate.mockResolvedValueOnce({});
       findModal().vm.$emit('primary', { preventDefault: jest.fn() });
       expect(mutate).toHaveBeenCalledWith({
         mutation: expect.any(Object),
-        variables: { oncallScheduleRotationCreate: expect.objectContaining({ projectPath }) },
+        variables: { OncallRotationCreateInput: expect.objectContaining({ projectPath }) },
       });
     });
 
     it('does not hide the rotation modal and shows error alert on fail', async () => {
       const error = 'some error';
-      mutate.mockResolvedValueOnce({ data: { oncallScheduleRotationCreate: { errors: [error] } } });
+      mutate.mockResolvedValueOnce({ data: { oncallRotationCreate: { errors: [error] } } });
       findModal().vm.$emit('primary', { preventDefault: jest.fn() });
       await waitForPromises();
       expect(mockHideModal).not.toHaveBeenCalled();
@@ -195,7 +198,5 @@ describe('AddRotationModal', () => {
       await awaitApolloDomMock();
       expect(userSearchQueryHandler).toHaveBeenCalledWith({ search: 'root' });
     });
-
-    // TODO: Once the BE is complete for the mutation add specs here for that via a creationHandler
   });
 });
