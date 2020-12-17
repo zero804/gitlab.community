678e2b3ae2cff4f1f69f28daec1a02cb48c33551
wortschi <mwortschack@gitlab.com>
2020-12-17 08:57:01 +0100

    Deep link to DevOps Adoption tab
    - Mark tab as active based
    on presence of tab query param

---
+++ ee/app/assets/javascripts/admin/dev_ops_report/tabs.js
@@ -0,0 +1,24 @@
+import { historyPushState } from '~/lib/utils/common_utils';
+import { mergeUrlParams } from '~/lib/utils/url_utility';
+
+const DEVOPS_ADOPTION_PANE = 'devops-adoption-pane';
+
+const tabClickHandler = e => {
+  const { hash } = e.currentTarget;
+  const tab =
+    hash === `#${DEVOPS_ADOPTION_PANE}` ? DEVOPS_ADOPTION_PANE.replace(/-pane/, '') : null;
+  const newUrl = mergeUrlParams({ tab }, window.location.href);
+  historyPushState(newUrl);
+};
+
+const initTabs = () => {
+  const tabLinks = document.querySelectorAll('.js-devops-tab-item a');
+
+  if (tabLinks.length) {
+    tabLinks.forEach(tabLink => {
+      tabLink.addEventListener('click', e => tabClickHandler(e));
+    });
+  }
+};
+
+export default initTabs;
